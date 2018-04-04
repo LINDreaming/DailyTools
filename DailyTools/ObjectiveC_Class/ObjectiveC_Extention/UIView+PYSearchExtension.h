c839c298445af8d1be0bb6f5d6fdaebb2ff5cccd
林喜 <1477820229@qq.com>
2018-04-04 17:40:36 +0800

    更新pods

---
+++ DailyTools/ObjectiveC_Class/ObjectiveC_Extention/UIView+PYSearchExtension.h
@@ -0,0 +1,17 @@
+
+//  UIView 分类
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (PYSearchExtension)
+
+@property (nonatomic, assign) CGFloat py_x;
+@property (nonatomic, assign) CGFloat py_y;
+@property (nonatomic, assign) CGFloat py_centerX;
+@property (nonatomic, assign) CGFloat py_centerY;
+@property (nonatomic, assign) CGFloat py_width;
+@property (nonatomic, assign) CGFloat py_height;
+@property (nonatomic, assign) CGSize  py_size;
+@property (nonatomic, assign) CGPoint py_origin;
+
+@end
---
+++ Pods/Target Support Files/Pods-DailyTools/Pods-DailyTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DailyTools : NSObject
+@end
+@implementation PodsDummy_Pods_DailyTools
+@end
